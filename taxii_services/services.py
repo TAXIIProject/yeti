109449adff6841752f0626405a6a108996092787
Bryan Worrell <bworrell@mitre.org>
2013-06-30 22:59:05 -0400

    adding initial services module with stub classes

---
+++ taxii_services/services.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import libtaxii
+import dateutil.parser # parsers
+
+
+class TAXIIService(object):
+    pass
+
+
+class InboxService(TAXIIService):
+    pass
+
+
+class PollService(TAXIIService):
+    pass 
