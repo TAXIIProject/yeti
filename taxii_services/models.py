c18749472eaf7dee1608ba54dffbedf3d3e57bfc
Bryan Worrell <bworrell@mitre.org>
2013-07-02 16:10:42 -0400

    updated models to have name and description and created/updated date information where beneficial

--- taxii_services/models.py
+++ taxii_services/models.py
@@ -6,58 +6,109 @@ from django.contrib.auth.models import User
 from dateutil.tz import tzutc
 import datetime
 
+MAX_ID_LEN      = 128
+MAX_NAME_LEN    = 128
+
 class ProtocolBindingId(models.Model):
-    binding_id = models.CharField(max_length=256)
+    name = models.CharField(max_length=MAX_NAME_LEN, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    binding_id = models.CharField(max_length=MAX_ID_LEN)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     
     def __unicode__(self):
-        return u'%s' % (self.binding_id)
+        if self.name:
+            return u'%s' % (self.name)
+        else:
+            return u'%s' % (self.binding_id)
 
 class ContentBindingId(models.Model):
-    binding_id = models.CharField(max_length=256)
+    name = models.CharField(max_length=MAX_NAME_LEN, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    binding_id = models.CharField(max_length=MAX_ID_LEN)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
-        return u'%s' % (self.binding_id)
+        if self.name:
+            return u'%s' % (self.name)
+        else:
+            return u'%s' % (self.binding_id)
 
 class MessageBindingId(models.Model):
-    binding_id = models.CharField(max_length=256)
+    name = models.CharField(max_length=MAX_NAME_LEN, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    binding_id = models.CharField(max_length=MAX_ID_LEN)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
-        return u'%s' % (self.binding_id)
+        if self.name:
+            return u'%s' % (self.name)
+        else:
+            return u'%s' % (self.binding_id)
 
-class DataFeedPushMethods(models.Model):
+class DataFeedPushMethod(models.Model):
+    name = models.CharField(max_length=MAX_NAME_LEN, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     protocol_binding = models.ForeignKey(ProtocolBindingId)
     message_binding = models.ForeignKey(MessageBindingId)
     
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        if self.name:
+            return u'%s' % (self.name)
+        else:
+            return u'%s | %s' % (self.protocol_binding, self.message_binding)
+    
 class DataFeedPollInformation(models.Model):
+    name = models.CharField(max_length=MAX_NAME_LEN, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     address = models.URLField()
     protocol_binding = models.ForeignKey(ProtocolBindingId)
     message_bindings = models.ManyToManyField(MessageBindingId)
+    
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        if self.name:
+            return u'%s' % (self.name)
+        else:
+            return u'%s | %s' % (self.protocol_binding, self.message_binding)
     
     class Meta:
         ordering = ['address']
     
 class DataFeedSubscriptionMethod(models.Model):
+    name = models.CharField(max_length=MAX_NAME_LEN, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     address = models.URLField()
     protocol_binding = models.ForeignKey(ProtocolBindingId)
     message_bindings = models.ManyToManyField(MessageBindingId)
+    
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        if self.name:
+            return u'%s' % (self.name)
+        else:
+            return u'%s | %s' % (self.protocol_binding, self.message_binding)
     
     class Meta:
         ordering = ['address']
     
 class DataFeed(models.Model):    
-    name = models.CharField(max_length=128) # this will be used in the URL to access this feed
-    description = models.TextField()
+    name = models.CharField(max_length=MAX_NAME_LEN) # this will be used in the URL to access this feed
+    description = models.TextField(null=True, blank=True)
     users = models.ManyToManyField(User) # users allowed to access this data feed.
     unauthenticated = models.BooleanField(default=True) # allow unauthenticated access to this data feed
     
     supported_content_bindings = models.ManyToManyField(ContentBindingId)
-    push_methods = models.ManyToManyField(DataFeedPushMethods)
+    push_methods = models.ManyToManyField(DataFeedPushMethod)
     poll_service_instances = models.ManyToManyField(DataFeedPollInformation)
     subscription_methods = models.ManyToManyField(DataFeedSubscriptionMethod)
     
@@ -77,14 +128,14 @@ class ContentBlock(models.Model):
     data_feed = models.ManyToManyField(DataFeed)
     timestamp_label = models.DateTimeField(default=lambda:datetime.datetime.now(tzutc()))
     submitted_by = models.ForeignKey(User)#Not sure this is needed, but we track it anyway
-    message_id = models.CharField(max_length=128, null=True) # associated message id if present. is there always a 1-to-1 for message ids and content blocks
+    message_id = models.CharField(max_length=MAX_ID_LEN, null=True, blank=True) # associated message id if present. is there always a 1-to-1 for message ids and content blocks
     
     #TAXII Properties of a content block
     content_binding = models.ForeignKey(ContentBindingId)
     content = models.TextField()
     padding = models.TextField()
     
-    date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True) # look at this for problems with apache instances
     date_updated = models.DateTimeField(auto_now=True)
     
     def __unicode__(self):
@@ -94,7 +145,7 @@ class ContentBlock(models.Model):
         ordering = ['timestamp_label']
 
 class DataFeedSubscription(models.Model):
-    subscription_id = models.CharField(max_length=256, unique=True) # uri formatted subscription id
+    subscription_id = models.CharField(max_length=128, unique=True) # uri formatted subscription id
     user = models.ForeignKey(User)
     data_feed = models.ForeignKey(DataFeed)
     data_feed_method = models.ForeignKey(DataFeedSubscriptionMethod)
