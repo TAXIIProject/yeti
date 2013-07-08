e7f971d05849e1365b7a6a08a1ad6c567a1458d9
Bryan Worrell <bworrell@mitre.org>
2013-07-08 17:13:51 -0400

    beginning to flesh out inbox and poll service view methods

--- taxii_services/views.py
+++ taxii_services/views.py
@@ -12,14 +12,30 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
+import taxii_services.utils.handlers as handlers
+from taxii_services.models import DataFeed, ContentBlock
+import libtaxii.messages as tm
+
+
 
 @csrf_exempt
-@login_required
-def inbox_service(dest_feed_name=None):
-    pass
+def inbox_service(request, dest_feed_name=None):
+    logger = logging.getLogger('yeti.taxii_services.views.inbox_service')
+    logger.info('entering inbox service')
+    
+    resp = handlers.validate_taxii_request(request)
+    if resp: return resp
+
+    
+
+    message = tm.get_message_from_xml(request.body)
 
 @csrf_exempt
-@login_required
-def poll_service():
-    pass
-
+def poll_service(request, feed_name):
+    logger = logging.getLogger("yeti.taxii_services.views.poll_service")
+    logger.info('entering poll service')
+    
+    resp = handlers.validate_taxii_request(request)
+    if resp: return resp
+    
+    message = tm.get_message_from_xml(request.body)
