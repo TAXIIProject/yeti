0790a2589b846511e658b617f57cc408711beea4
Bryan Worrell <bworrell@mitre.org>
2013-07-10 18:01:07 -0400

    modified url patterns

--- taxii_services/urls.py
+++ taxii_services/urls.py
@@ -1,23 +1,9 @@
 # Copyright (C) 2013 - The MITRE Corporation
 # For license information, see the LICENSE.txt file
 
-from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'yeti.views.home', name='home'),
-    # url(r'^yeti/', include('yeti.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    
-    #url(r'^(inbox)/$', 'taxii_services.inbox_service'),
-    #url(r'^(inbox)/(?P<dest_feed_name>\s+)/$', 'taxi_services.inbox_service'),
+    url(r'^inbox/(\w+)/$', 'taxii_services.views.inbox_service'),
+    url(r'^poll/$', 'taxii_services.views.poll_service'),
 )
--- yeti/urls.py
+++ yeti/urls.py
@@ -5,16 +5,12 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'yeti.views.home', name='home'),
-    # url(r'^home/', 'views.home'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^services/', include('taxii_services.urls')),
+    #url(r'^test/', 'taxii_services.views.test')
 )
+
