006258128a0f792ae6f4840827f83d4186179c16
MarkDavidson <mdavidson@mitre.org>
2013-07-11 10:39:21 -0400

    Added ContentBlockAdmin and removed tabs

--- taxii_services/admin.py
+++ taxii_services/admin.py
@@ -2,28 +2,31 @@
 # For license information, see the LICENSE.txt file
 
 from django.contrib import admin
-from taxii_services.models import DataFeed, MessageBindingId, ContentBindingId, ProtocolBindingId, DataFeedPushMethod, DataFeedPollInformation, DataFeedSubscriptionMethod
+from taxii_services.models import DataFeed, MessageBindingId, ContentBindingId, ContentBlock, ProtocolBindingId, DataFeedPushMethod, DataFeedPollInformation, DataFeedSubscriptionMethod
 
 
 class DataFeedAdmin(admin.ModelAdmin):
-	list_display = ['id', 'name', 'description']
-	
+    list_display = ['id', 'name', 'description']
+    
 class ProtocolBindingIdAdmin(admin.ModelAdmin):
-	list_display = ['title', 'description', 'id', 'binding_id', 'date_created', 'date_updated']
+    list_display = ['title', 'description', 'id', 'binding_id', 'date_created', 'date_updated']
 
 class MessageBindingIdAdmin(admin.ModelAdmin):
-	list_display = ['title', 'description', 'id', 'binding_id', 'date_created', 'date_updated']
-	
+    list_display = ['title', 'description', 'id', 'binding_id', 'date_created', 'date_updated']
+    
 class ContentBindingIdAdmin(admin.ModelAdmin):
-	list_display = ['title', 'description', 'id', 'binding_id', 'date_created', 'date_updated']
-	
-class DataFeedPushMethodAdmin(admin.ModelAdmin):
-	pass
+    list_display = ['title', 'description', 'id', 'binding_id', 'date_created', 'date_updated']
 
+class ContentBlockAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title','description', 'timestamp_label','submitted_by','message_id','content_binding','content','padding','date_created','date_updated']
+    
+class DataFeedPushMethodAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(DataFeed, DataFeedAdmin)
 admin.site.register(MessageBindingId, MessageBindingIdAdmin)
 admin.site.register(ContentBindingId, ContentBindingIdAdmin)
+admin.site.register(ContentBlock, ContentBlockAdmin)
 admin.site.register(ProtocolBindingId, ProtocolBindingIdAdmin)
 admin.site.register(DataFeedPushMethod, DataFeedPushMethodAdmin)
 admin.site.register(DataFeedPollInformation)
