ad4d2be1f751ed4a5f9faa25c325542c999be1f2
MarkDavidson <mdavidson@mitre.org>
2013-07-11 09:59:51 -0400

    Updated to use the use_https portion of create_taxii_response

--- taxii_services/views.py
+++ taxii_services/views.py
@@ -28,18 +28,18 @@ def inbox_service(request, dest_feed_name):
     except Exception as ex:
         logger.debug('unable to parse inbound message: %s' % (ex.message))
         m = tm.StatusMessage(tm.generate_message_id(), '0', status_type=tm.ST_BAD_MESSAGE, message='Message received could not be parsed')
-        return handlers.create_taxii_response(m)
+        return handlers.create_taxii_response(m, use_https=request.is_secure())
     
     if taxii_message.message_type != tm.MSG_INBOX_MESSAGE:
         logger.info('taxii taxii_message [%s] was not inbox type [%s]' % (taxii_message.message_id, taxii_message.message_type))
         m = tm.StatusMessage(tm.generate_message_id(), taxii_message.message_id, status_type=tm.ST_FAILURE, message='Message sent to inbox service did not have an inbox taxii_message type')
-        return handlers.create_taxii_response(m)
+        return handlers.create_taxii_response(m, use_https=request.is_secure())
     
     dest_data_feed = DataFeed.objects.filter(name=dest_feed_name)
     if not dest_data_feed:
         logger.debug('attempting to assign content to unknown data feed [%s]' % (dest_feed_name))
         m = tm.StatusMessage(tm.generate_message_id(), taxii_message.message_id, status_type=tm.ST_NOT_FOUND, message='Destination feed does not exist [%s]' % (dest_feed_name))
-        return handlers.create_taxii_response(m)
+        return handlers.create_taxii_response(m, use_https=request.is_secure())
     
     logger.debug('taxii message [%s] contains [%d] content blocks' %(taxii_message.message_id, len(taxii_message.content_blocks)))
     for inbox_block in taxii_message.content_blocks:
@@ -58,7 +58,7 @@ def inbox_service(request, dest_feed_name):
             c.save()
     
     m = tm.StatusMessage(tm.generate_message_id(), taxii_message.message_id, status_type = tm.ST_SUCCESS)
-    return handlers.create_taxii_response(m)
+    return handlers.create_taxii_response(m, use_https=request.is_secure())
 
 @csrf_exempt
 @yeti_auth_check
@@ -76,18 +76,18 @@ def poll_service(request):
     except Exception as ex:
         logger.debug('unable to parse inbound message: %s' % (ex.message))
         m = tm.StatusMessage(tm.generate_message_id(), '0', status_type=tm.ST_BAD_MESSAGE, message='Message received could not be parsed')
-        return handlers.create_taxii_response(m)
+        return handlers.create_taxii_response(m, use_https=request.is_secure())
     
     if taxii_message.message_type != tm.MSG_POLL_REQUEST:
         logger.info('Message [%s] was not poll request [%s]' % (taxii_message.message_id,taxii_message.message_type))
         m = tm.StatusMessage(tm.generate_message_id(), taxii_message.message_id, status_type=tm.ST_FAILURE, message='Message sent to poll service did not have a poll request message type')
-        return handlers.create_taxii_response(m)    
+        return handlers.create_taxii_response(m, use_https=request.is_secure())    
     
     data_feed = DataFeed.objects.filter(name=taxii_message.feed_name)
     if not data_feed:
         logger.debug('attempting to poll unknown data feed [%s]' % (taxii_message.feed_name))
         m = tm.StatusMessage(tm.generate_message_id(), taxii_message.message_id, status_type=tm.ST_NOT_FOUND, message='Data feed does not exist [%s]' % (taxii_message.feed_name))
-        return handlers.create_taxii_response(m)
+        return handlers.create_taxii_response(m, use_https=request.is_secure())
     
     # build query for poll results
     query_params = {'data_feeds' : data_feed}
@@ -129,6 +129,6 @@ def poll_service(request):
         
         poll_response_message.content_blocks.append(cb)
     
-    return handlers.create_taxii_response(poll_response_message)
+    return handlers.create_taxii_response(poll_response_message, use_https=request.is_secure())
 
 
