99d27b911ff7683b8d53df68183d1dc2747a7597
Bryan Worrell <bworrell@mitre.org>
2013-08-09 12:15:38 -0400

    parsed input datetime information for begin/end timestamp into datetime objects

--- scripts/poll_client.py
+++ scripts/poll_client.py
@@ -5,12 +5,14 @@
 import sys
 
 from optparse import OptionParser
+import dateutil.parser
 
 import libtaxii as t
 import libtaxii.messages as tm
 import libtaxii.clients as tc
 
-def main():
+
+def main():    
     parser = OptionParser()
     parser.add_option("--host", dest="host", default="localhost", help="Host where the Poll Service is hosted. Defaults to localhost.")
     parser.add_option("--port", dest="port", default="8080", help="Port where the Poll Service is hosted. Defaults to 8080.")
@@ -20,13 +22,16 @@ def main():
     parser.add_option("--end_timestamp", dest="end_ts", default=None, help="The end timestamp (format: YYYY-MM-DDTHH:MM:SS.ssssss+/-hh:mm) for the poll request. Defaults to None.")
 
     (options, args) = parser.parse_args()
-
-    poll_req = tm.PollRequest(message_id = tm.generate_message_id(),
-                              feed_name = options.feed,
-                              exclusive_begin_timestamp_label = options.begin_ts,
-                              inclusive_end_timestamp_label = options.end_ts)
+    
+    begin_ts    = dateutil.parser.parse(options.begin_ts) if options.begin_ts else None
+    end_ts      = dateutil.parser.parse(options.end_ts) if options.begin_ts else None
+    
+    poll_req    = tm.PollRequest(message_id = tm.generate_message_id(),
+                                 feed_name = options.feed,
+                                 exclusive_begin_timestamp_label = begin_ts,
+                                 inclusive_end_timestamp_label = end_ts)
+    
     poll_req_xml = poll_req.to_xml()
-
     print "Poll Request: \r\n", poll_req_xml
     client = tc.HttpClient()
     client.setProxy('noproxy')
