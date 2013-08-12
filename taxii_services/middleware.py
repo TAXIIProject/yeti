2cb117a154016eb58bd325f96e3d3aaf4607dce9
Bryan Worrell <bworrell@mitre.org>
2013-08-12 14:29:43 -0400

    server logs exception messages on internal server error. Closes #4

---
+++ taxii_services/middleware.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2013 - The MITRE Corporation
+# For license information, see the LICENSE.txt file
+
+import logging
+from django.http import HttpResponseServerError
+
+import libtaxii.messages as tm
+import taxii_services.utils.handlers as handlers
+
+class ProcessExceptionMiddleware(object):
+    def process_exception(self, request, exception):
+        logger = logging.getLogger('yeti.taxii_services.middleware.ProcessExceptionMiddleware.process_exception')
+        logger.debug('server error occured: %s' % (str(exception)) )
+    
+        if request.path.startswith('/services'):
+            logger.debug('returning ST_FAILURE message')
+            m = tm.StatusMessage(tm.generate_message_id(), '0', status_type=tm.ST_FAILURE, message='An internal server error occurred')
+            return handlers.create_taxii_response(m, handlers.HTTP_STATUS_OK, use_https=request.is_secure()) # should the http status be 500?
+
+        resp = HttpResponseServerError()
+        resp.body = 'A server error occurred'
+        return resp
--- taxii_services/urls.py
+++ taxii_services/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^inbox/(\w+)/$', 'taxii_services.views.inbox_service'),
     url(r'^poll/$', 'taxii_services.views.poll_service'),
+    url(r'^discovery/$', 'taxii_services.views.discovery_service'),
 )
