a5683601c274edf4c0dce5c06e1586c8a9e6d6e5
Bryan Worrell <bworrell@mitre.org>
2013-09-24 09:14:23 -0400

    Added normalize() method to taxii_services.utils that normalizes all whitespace of an input string to one space and strips leading/trailing whitespace

--- taxii_services/utils/__init__.py
+++ taxii_services/utils/__init__.py
@@ -10,3 +10,10 @@ def get_source_ip(request):
     ip = x_header.split(',')[0] if x_header else request.META.get('REMOTE_ADDR')
     
     return ip
+
+
+def normalize(str_):
+    """Normalizes all whitespace in string to a single whitespace and 
+    strips leading/trailing whitespace.
+    """
+    return ' '.join(str(str_).split()) if str_ is not None else None
