5216ff26964b1701563c782cb010263b749a1b4c
MarkDavidson <mdavidson@mitre.org>
2014-05-01 14:58:46 -0400

    Added support for asynch responses for queries

--- taxii_services/handlers.py
+++ taxii_services/handlers.py
@@ -6,7 +6,7 @@ import datetime
 from dateutil.tz import tzutc
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
-from taxii_services.models import Inbox, DataCollection, ContentBlock, ContentBindingId
+from taxii_services.models import Inbox, DataCollection, ContentBlock, ContentBindingId, ResultSet
 from taxii_services.utils import make_safe
 import libtaxii as t
 import libtaxii.messages_11 as tm11
@@ -14,6 +14,8 @@ import libtaxii.taxii_default_query as tdq
 from lxml import etree
 import StringIO
 import query_helpers as qh
+import re
+import os.path
 
 
 # A set of headers that are utilized by TAXII. These are formatted as Django's
@@ -315,6 +317,54 @@ def query_get_content(request, taxii_message):
     
     logger.debug('[%d] content blocks match query from data collection [%s]', len(matching_content_blocks), make_safe(data_collection.name))
     
+    # Now that we've determined which Content Blocks match the query, 
+    # determine whether an Asynchronous Poll will be used.
+    #
+    # At this point, from a technical perspective, the results are complete and could be 
+    # send back to the requester. However, in order to demonstrate the spec, artificial
+    # parameters are used to determine whether to use an Asynchronous Poll.
+    
+    # Here is the super advanced and complex application logic to
+    # determine whether the response should be asynchronous
+    # modify at your peril
+    asynch = False
+    text = 'NOT '
+    if len(matching_content_blocks) > 2:
+        asynch = True
+        text = ''
+    
+    logger.debug('Asyncronous Poll Response will %sbe used', text)
+    
+    if(asynch):#Send a status message of PENDING and store the results in the DB
+        #Check if the client support asynchronous polling
+        if taxii_message.poll_parameters.allow_asynch is not True:#We want to do asynch but client doesn't support it. Error
+            logger.debug('Client does not support asynch response. Returning FAILURE Status Message.')
+            m = tm11.StatusMessage(tm11.generate_message_id(), 
+                                   taxii_message.message_id, 
+                                   status_type=tm11.ST_FAILURE, 
+                                   message='You did not indicate support for Asynchronous Polling, but the server could only send an asynchronous response')
+            return create_taxii_response(m, use_https=request.is_secure())
+        
+        #Create the result ID
+        #The result_id ends up looking something like '2014_05_01T12_56_24_403000'
+        #This production can be conveniently validated with a regex of ^\d{4}_\d{2}_\d{2}T\d{2}_\d{2}_\d{2}_\d{6}$
+        result_id = datetime.datetime.now().isoformat().replace(':','_').replace('-','_').replace('.','_')
+        rs = ResultSet()
+        rs.result_id = result_id
+        rs.data_collection = data_collection
+        rs.save()
+        rs.content_blocks.add(*matching_content_blocks)
+        
+        m = tm11.StatusMessage(tm11.generate_message_id(),
+                               taxii_message.message_id,
+                               status_type = tm11.ST_PENDING,
+                               message='Your response is pending. Please request it again at a later time.',
+                               status_detail = {'ESTIMATED_WAIT': '0', 'RESULT_ID': result_id, 'WILL_PUSH': False})
+        logger.debug('Responding with status message of PENDING, result_id = %s', result_id)
+        return create_taxii_response(m, use_https=request.is_secure())
+    
+    logger.debug('Returning Poll Response')
+    
     # build poll response
     poll_response_message = tm11.PollResponse(tm11.generate_message_id(), 
                                               taxii_message.message_id, 
@@ -331,6 +381,52 @@ def query_get_content(request, taxii_message):
             poll_response_message.content_blocks.append(cb)
     
     return create_taxii_response(poll_response_message, use_https=request.is_secure())
+
+def query_fulfillment(request, taxii_message):
+    """
+    Returns a Poll response for a given Poll Fulfillment Request.
+    
+    This method attempts to locate the result id and return it to the requestor.
+    No authentication or authorization checks are performed.
+    """
+    logger = logging.getLogger('taxii_services.utils.handlers.query_get_content')
+    
+    r = re.compile('^\d{4}_\d{2}_\d{2}T\d{2}_\d{2}_\d{2}_\d{6}$')
+    if not r.match(taxii_message.result_id):#The ID doesn't match - error
+        logger.debug('The request result_id did not match the validation regex')
+        m = tm11.StatusMessage(tm11.generate_message_id(),
+                               taxii_message.message_id,
+                               status_type = tm11.NOT_FOUND,
+                               message='The requested Result ID was not found.')
+        return create_taxii_response(m, use_https=request.is_secure())
+    
+    try:
+        rs = ResultSet.objects.get(result_id = taxii_message.result_id)
+    except: #Not found
+        logger.debug('The request result_id was not found in the database')
+        m = tm11.StatusMessage(tm11.generate_message_id(),
+                               taxii_message.message_id,
+                               status_type = tm11.ST_NOT_FOUND,
+                               message='The requested Result ID was not found.')
+        return create_taxii_response(m, use_https=request.is_secure())
+    
+    poll_response = tm11.PollResponse(tm11.generate_message_id(), 
+                                      taxii_message.message_id,
+                                      collection_name = rs.data_collection.name)
+    if rs.begin_ts is not None:
+        poll_response.exclusive_begin_timestamp_label = rs.begin_ts
+    if rs.end_ts is not None:
+        poll_response.inclusive_end_timestamp_label = rs.end_ts
+    
+    print 'got %s content blocks' % len(rs.content_blocks.all())
+    for block in rs.content_blocks.all():
+        cb = tm11.ContentBlock(block.content_binding.binding_id, block.content)
+        poll_response.content_blocks.append(cb)
+    
+    rs.delete()
+    
+    logger.debug('Returning the result. The result set has been deleted.')
+    return create_taxii_response(poll_response, use_https=request.is_secure())
 
 def discovery_get_services(request, taxii_message):
     """Returns a Discovery response for a given Discovery Request Message"""
