458e92bc259f19242fcec091026eeef4e42d318a
MarkDavidson <mdavidson@mitre.org>
2014-06-02 10:03:32 -0400

    Updated to use taxii_web_utils

--- taxii_services/decorators.py
+++ taxii_services/decorators.py
@@ -2,8 +2,9 @@
 # For license information, see the LICENSE.txt file
 
 import taxii_services.settings
-import taxii_services.handlers as handlers
 import libtaxii.messages_11 as tm11
+import taxii_services.handlers as handlers
+import taxii_web_utils.response_utils as response_utils
 
 def taxii_auth_check(func):
     """
@@ -19,6 +20,6 @@ def taxii_auth_check(func):
             return func(request, *args, **kwargs)
         
         m = tm11.StatusMessage(tm11.generate_message_id(), '0', status_type=tm.ST_UNAUTHORIZED, message='You are not authorized to access this URL.')
-        return handlers.create_taxii_response(m)
+        return response_utils.create_taxii_response(m.to_xml(), handlers.get_headers('1.1', request.is_secure()))
     
     return inner
