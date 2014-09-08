8d7cd4482d61ad90fbbef7fa48042cfec418bd8c
MarkDavidson <mdavidson@mitre.org>
2014-09-08 14:10:56 -0400

    Version bump to 2.0 alpha

--- yeti/__init__.py
+++ yeti/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # For license information, see the LICENSE.txt file
 
-__version__ = "1.1.0"
+__version__ = "2.0a"
 
 import StixXml111QueryHandler
 
