289bf4c696e4d8fcfb1cecbea9222fb78c9d9536
MarkDavidson <mdavidson@mitre.org>
2014-10-22 12:55:45 -0400

    Version bump

--- yeti/__init__.py
+++ yeti/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # For license information, see the LICENSE.txt file
 
-__version__ = "2.0a"
+__version__ = "2.0a3"
 
 import taxii_services
 taxii_services.register_admins()
