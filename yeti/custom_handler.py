b428c8f826628ebf674689a5e24478c0b64809f1
MarkDavidson <mdavidson@mitre.org>
2014-12-17 13:31:25 -0500

    Closes #77.

--- yeti/custom_handler.py
+++ yeti/custom_handler.py
@@ -1,36 +1,30 @@
 # Copyright (C) 2013 - The MITRE Corporation
 # For license information, see the LICENSE.txt file
 
-import taxii_services.taxii_handlers as th
-import taxii_services.handlers as h
+import taxii_services as ts
+from taxii_services.message_handlers.base_handlers import BaseMessageHandler
 import libtaxii.messages_11 as tm11
 
-class MyCustomHandler(th.MessageHandler):
+
+class MyCustomHandler(BaseMessageHandler):
     """
     Does the same thing as the Built-in Discovery Request Handler,
     plus prints a statement.
     Just an example of how to extend MessageHandler
     """
+
+    #Indicate to taxii_services which request messages are supported by this handler
     supported_request_messages = [tm11.DiscoveryRequest]
+
+    #Indicate to taxii_services the version of this message handler
+    version = "1"
     
     @staticmethod
-    def handle_message(discovery_service, discovery_request, django_request):        
+    def handle_message(discovery_service, discovery_request, django_request):
+        # Your custom code would go in here.
         print "MyCustomHandler is being called"
         # Chain together all the enabled services that this discovery service advertises
-        advertised_services = list(chain(discovery_service.advertised_discovery_services.filter(enabled=True),
-                                         discovery_service.advertised_poll_services.filter(enabled=True),
-                                         discovery_service.advertised_inbox_services.filter(enabled=True),
-                                         discovery_service.advertised_collection_management_services.filter(enabled=True)))
-        
-        # Create the stub DiscoveryResponse
-        discovery_response = tm11.DiscoveryResponse(tm11.generate_message_id(), discovery_request.message_id)
-        
-        # Iterate over advertised services, creating a service instance for each
-        for service in advertised_services:
-            service_instances = taxiifiers.service_to_service_instances(service)
-            discovery_response.service_instances.extend(service_instances)
-        
-        # Return the Discovery Response
+        discovery_response = discovery_service.to_discovery_response_11(discovery_request.message_id)
         return discovery_response
 
-#h.register_message_handler(MyCustomHandler, name="MySuperHandler")
+#ts.register_message_handler(MyCustomHandler, name="MySuperHandler")
