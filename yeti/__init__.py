332f4b8cc464852f0df8984f15b89f06c3cee638
Greg Back <gback@mitre.org>
2015-03-26 12:33:41 -0500

    Bump verison to 2.0a5

--- yeti/__init__.py
+++ yeti/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # For license information, see the LICENSE.txt file
 
-__version__ = "2.0a3"
+__version__ = "2.0a5"
 
 import taxii_services
 taxii_services.register_admins()
